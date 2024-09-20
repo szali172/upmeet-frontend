bf76a302e55fef8ea955c4cdc113fc416e6b0f0c
Adam Palmer <adampalmer@rocketmortgage.com>
2024-09-19 20:15:01 -0400

    Changed the Environment Name

--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { Favorite } from '../models/favorite';
-import { environment } from '../../environments/environments';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
